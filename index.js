eb4b13b545115c4ac9e82676712058451f5f6a15
Min-Tri <tmtri.290798@gmail.com>
2026-01-05 21:29:17 +0700

    solve

--- index.js
+++ index.js
@@ -24,7 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  const dateParam = req.params.date;
 
+  let date;
+
+  if (!dateParam) {
+    date = new Date();
+  }
+  else if (!isNaN(dateParam)) {
+    date = new Date(Number(dateParam));
+  }
+  else {
+    date = new Date(dateParam);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
